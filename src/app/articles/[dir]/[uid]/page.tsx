183a0533f63ce8c541f52a2e250f420046703829
larry <xspanni@gmail.com>
2025-02-12 16:48:21 +0800

    隐藏文章附件卡片，不再展示

--- src/app/articles/[dir]/[uid]/page.tsx
+++ src/app/articles/[dir]/[uid]/page.tsx
@@ -95,8 +95,8 @@ export default async function Home({params, searchParams}: {
                     </div>
                 </div>
                 <div className={'rightArea'}>
-                    <ArticleAssets channelUrn={''} articleUrn={''}/>
                     <TocInfo readurl={readUrl} model={tocList}/>
+                    {/*<ArticleAssets channelUrn={''} articleUrn={''}/>*/}
                 </div>
             </div>
             <GoTop anchor={templateBodyId}/>
